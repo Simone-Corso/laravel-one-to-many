5aba842978188fe531601af40e135c8e295ec208
Simone-Corso <Scorso26@gmail.com>
2024-02-15 23:20:05 +0100

    ho eseguito con successo il migrate

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
+use App\Models\TypeDetail;
 
 class Type extends Model
 {
     use HasFactory;
+
+    public function projects(){
+        $this->hasMany(Project::class);
+    }
+
 }
