21ddd2b2e151bedb1d79c60acf99cce494a07475
Simone-Corso <Scorso26@gmail.com>
2024-02-15 23:30:22 +0100

    ho inserito nel project model la parte del type.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
 
 class Project extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    public function type(){
+        return $this->belongsto(Type::class);
+    }
 }
